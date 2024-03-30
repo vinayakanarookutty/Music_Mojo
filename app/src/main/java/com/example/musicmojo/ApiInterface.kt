bd9c2ffa52db160763ca4b878cd1ad458e67b62a
Vinayak S <vinayaksukhalal@gmail.com>
2024-03-30 22:29:42 +0530

    first commit

---
+++ app/src/main/java/com/example/musicmojo/ApiInterface.kt
@@ -0,0 +1,11 @@
+package com.example.musicmojo
+
+import retrofit2.Call
+import retrofit2.http.Query
+import retrofit2.http.GET
+import retrofit2.http.Headers
+interface ApiInterface {
+    @Headers("X-RapidAPI-Key: 3dc707c69emsh9e12bded8711213p1dd3a4jsn335389edff6d" + "X-RapidAPI-Host:deezerdevs-deezer.p.rapidapi.com")
+    @GET("search")
+    fun getData(@Query("q")query: String): Call<MyData>
+}
---
+++ app/src/main/java/com/example/musicmojo/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.musicmojo
+
+import android.nfc.Tag
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val retrofitBuilder=Retrofit.Builder()
+            .baseUrl("https://deezerdevs-deezer.p.rapidapi.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiInterface::class.java)
+        val retrofitData=retrofitBuilder.getData("eminem")
+        retrofitData.enqueue(object : Callback<MyData?> {
+            override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
+             val dataList=response.body()?.data
+                val textView=findViewById<TextView>(R.id.helloText)
+                textView.text=dataList.toString()
+                Log.d("TAG:onResponse", "onResponse: "+response.body())
+            }
+
+            override fun onFailure(call: Call<MyData?>, t: Throwable) {
+                Log.d("TAG:onFailure", "onFailure: "+t.message)
+            }
+        })
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
