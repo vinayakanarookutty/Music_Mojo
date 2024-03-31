7b5efe511e41e8e4e2c78b68635089caf93dfa18
Vinayak S <vinayaksukhalal@gmail.com>
2024-03-31 10:21:19 +0530

    first commit

--- app/src/main/java/com/example/musicmojo/MainActivity.kt
+++ app/src/main/java/com/example/musicmojo/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.musicmojo
 
-import android.nfc.Tag
+
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
@@ -13,7 +13,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
